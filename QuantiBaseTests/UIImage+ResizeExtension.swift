3f36517adfc369fc7d54dffb16efff6dc7d41356
Jakub Prusa <jakub.prusa@quanti.cz>
2017-08-25 23:44:36 +0200

    UNIT: Added unit test bundle

---
+++ QuantiBaseTests/UIImage+ResizeExtension.swift
@@ -0,0 +1,35 @@
+//
+//  UIImage+ResizeExtension.swift
+//  QuantiBase
+//
+//  Created by Jakub Prusa on 8/25/17.
+//  Copyright © 2017 David Nemec. All rights reserved.
+//
+
+import XCTest
+import Q
+class UIImage_ResizeExtension: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testResize() {
+        func testImageResize() {
+            let originalImage = #imageLiteral(resourceName: "close")
+
+            let scaledImage = originalImage.with(newSize: CGSize.init(width: 200, height: 200))
+
+            XCTAssertEqual(scaledImage.size.width, 200)
+            XCTAssertEqual(scaledImage.size.height, 200)
+        }
+    }
+
+    
+}
