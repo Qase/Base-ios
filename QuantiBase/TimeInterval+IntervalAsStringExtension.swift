fd0ea5b33ff51d079683a835bc10ce94c2db04b0
Jakub Prusa <jakub.prusa@quanti.cz>
2017-08-26 22:12:27 +0200

    FEAT: Added new function to Time Interval formatter which is by latest guidelines. Old function is anotated as depracated. Added unit test.

--- QuantiBase/TimeInterval+IntervalAsStringExtension.swift
+++ QuantiBase/TimeInterval+IntervalAsStringExtension.swift
@@ -16,6 +16,7 @@ extension Double {
     ///
     /// - Parameter format: desired format
     /// - Returns: format NSTimeInterval as String
+    @available(*, deprecated)
     public func timeIntervalAsString(format: String = "hh:mm:ss:sss") -> String {
 
         let ms      = Int(self.truncatingRemainder(dividingBy: 1.0) * 1000)
@@ -31,5 +32,26 @@ extension Double {
         value       = value.replace("ss", replacement: String(format: "%0.2d", s))
         return value
     }
-    
+
+    /// Format NSTimeInterval to nice format.
+    /// Custom function which is similar to NSDateFormatter
+    /// Default format is hh:mm:ss:sss
+    ///
+    /// - Parameter format: desired format
+    /// - Returns: format NSTimeInterval as String
+    public func format(_ format: String = "hh:mm:ss:sss") -> String {
+
+        let ms      = Int(self.truncatingRemainder(dividingBy: 1.0) * 1000)
+        let asInt   = NSInteger(self)
+        let s       = asInt % 60
+        let m       = (asInt / 60) % 60
+        let h       = (asInt / 3600)
+
+        var value   = format
+        value       = value.replace("hh", replacement: String(format: "%0.2d", h))
+        value       = value.replace("mm", replacement: String(format: "%0.2d", m))
+        value       = value.replace("sss", replacement: String(format: "%0.3d", ms))
+        value       = value.replace("ss", replacement: String(format: "%0.2d", s))
+        return value
+    }
 }
---
+++ QuantiBaseTests/TimeInterval+IntervalAsStringExtension.swift
@@ -0,0 +1,56 @@
+//
+//  TimeInterval+IntervalAsStringExtension.swift
+//  QuantiBase
+//
+//  Created by Jakub Prusa on 26.08.17.
+//  Copyright © 2017 David Nemec. All rights reserved.
+//
+
+import XCTest
+@testable import QuantiBase
+
+class TimeInterval_IntervalAsStringExtension: XCTestCase {
+    
+
+    func testDeprecatedFormatter() {
+        let timeInterval = TimeInterval(exactly: 98765)
+
+        let test1  = timeInterval?.timeIntervalAsString()
+        XCTAssertEqual(test1, "27:26:05:000", "Error in default formatter")
+
+        let test2  = timeInterval?.timeIntervalAsString(format: "hh")
+        XCTAssertEqual(test2, "27", "Error in hour formatter")
+
+        let test3  = timeInterval?.timeIntervalAsString(format: "mm")
+        XCTAssertEqual(test3, "26", "Error in minute formatter")
+
+        let test4  = timeInterval?.timeIntervalAsString(format: "ss")
+        XCTAssertEqual(test4, "05", "Error in second formatter")
+
+        let test5  = timeInterval?.timeIntervalAsString(format: "hh_testing_ss")
+        XCTAssertEqual(test5, "27_testing_05", "Error with custom word in format string")
+
+    }
+
+
+    func testNewFormatter() {
+        let timeInterval = TimeInterval(exactly: 98765)
+
+        let test1  = timeInterval?.format()
+        XCTAssertEqual(test1, "27:26:05:000", "Error in default formatter")
+
+        let test2  = timeInterval?.format("hh")
+        XCTAssertEqual(test2, "27", "Error in hour formatter")
+
+        let test3  = timeInterval?.format("mm")
+        XCTAssertEqual(test3, "26", "Error in minute formatter")
+
+        let test4  = timeInterval?.format("ss")
+        XCTAssertEqual(test4, "05", "Error in second formatter")
+
+        let test5  = timeInterval?.format("hh_testing_ss")
+        XCTAssertEqual(test5, "27_testing_05", "Error with custom word in format string")
+    }
+
+    
+}
