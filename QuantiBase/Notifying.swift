1ea79c3a65e61efa32c5e839d8195cd1216bd85b
Martin Troup <martin.troup@quanti.cz>
2017-12-28 17:19:52 +0100

    REF: Since iOS 9 removing observers of NotificationCenter notifications is no longer necessary.

--- QuantiBase/Notifying.swift
+++ QuantiBase/Notifying.swift
@@ -9,15 +9,6 @@
 import UIKit
 
 /// Notifying protocol should be implemented by every class that registeres some Notification observers. 
-/// removeNotifications() method of the protocol should be called within deinit() method of the class that implements the protocol so 
-/// all registered observers are removed before the instance is released which prevents from memory leaks.
 public protocol Notifying {
     func registerForNotifications()
-    func removeNotifications()
-}
-
-extension Notifying {
-    public func removeNotifications() {
-        NotificationCenter.default.removeObserver(self)
-    }
 }
