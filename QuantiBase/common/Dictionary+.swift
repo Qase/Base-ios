d175abaaab5c399a2e10f548b19a2e0d367e2559
Martin Troup <martin.troup@quanti.cz>
2019-05-22 12:34:00 +0200

    FEAT: New extensions for Dictionary and Double.

---
+++ QuantiBase/common/Dictionary+.swift
@@ -0,0 +1,18 @@
+//
+//  Dictionary+.swift
+//  QuantiBase
+//
+//  Created by Martin Troup on 22/05/2019.
+//  Copyright © 2019 David Nemec. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+    /// Returns key-value tuple on given index.
+    ///
+    /// - Parameter i: index
+    subscript(i: Int) -> (key: Key, value: Value) {
+        return self[index(startIndex, offsetBy: i)]
+    }
+}
---
+++ QuantiBase/common/Double+.swift
@@ -0,0 +1,21 @@
+//
+//  Double+.swift
+//  QuantiBase
+//
+//  Created by Martin Troup on 22/05/2019.
+//  Copyright © 2019 David Nemec. All rights reserved.
+//
+
+import Foundation
+
+public extension Double {
+    /// Rounds the double to decimal places value
+
+    /// Returns String representation of self
+    ///
+    /// - Parameter places: number of places to be rounded to
+    /// - Returns: String representation of self
+    func string(roundedToPlaces places: Int) -> String {
+        return String(format: "%.\(places)f", self)
+    }
+}
