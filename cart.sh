8b78f97fc66d183fd88edd2f8d70d9b942310495
Finemas <proovaznik@gmail.com>
2021-05-04 12:15:12 +0200

    FEAT: update cart scrpt for using xcframewokrs

--- cart.sh
+++ cart.sh
@@ -1,3 +1,3 @@
 # This export fixes an issues with the latest Xcode beta.
 export XCODE_XCCONFIG_FILE=$PWD/tmp.xcconfig
-carthage update --platform iOS --no-use-binaries --cache-builds
+carthage update --platform iOS --no-use-binaries --cache-builds --use-xcframeworks
